a0f24d5e6a310137fde9ff4352da951e364f89f3
Joakim Lemb <joakim.lemb@idrettsforbundet.no>
2018-04-07 12:39:12 +0200

    initial commit

---
+++ AzureFileSync-Tier-Old-Files.ps1
@@ -0,0 +1,42 @@
+﻿$days_old = 14
+$path = "d:\afs01"
+
+Import-Module "C:\Program Files\Azure\StorageSyncAgent\StorageSync.Management.ServerCmdlets.dll"
+
+add-type -type  @'
+using System;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+ 
+namespace Disk
+{
+    public class SizeInfo
+    {
+        [DllImport("kernel32.dll", SetLastError=true, EntryPoint="GetCompressedFileSize")]
+        static extern uint GetCompressedFileSizeAPI(string lpFileName, out uint lpFileSizeHigh);
+ 
+        public static ulong GetCompressedFileSizeFunction(string FileName,bool SizeInMB)
+        {
+            uint HighOrder;
+            uint LowOrder;
+            LowOrder = GetCompressedFileSizeAPI(FileName, out HighOrder);
+            int error = Marshal.GetLastWin32Error();
+            if (HighOrder == 0 && LowOrder == 0xFFFFFFFF && error != 0)
+                throw new Win32Exception(error);
+            else
+                if (SizeInMB)
+                    return ((((ulong)HighOrder << 32) + LowOrder)/1024)/1024;
+                else
+                    return ((ulong)HighOrder << 32) + LowOrder;
+        }
+    }
+}
+'@
+
+$i = 0
+Get-ChildItem -File -Path "$path" -Recurse | Where-Object { ($_.LastWriteTime -lt (get-date).AddDays(-$days_old)) -and ([Disk.SizeInfo]::GetCompressedFileSizeFunction($_.FullName,$false) -gt 65536) } | % {
+    $i++
+    $_.FullName +" "+ [Disk.SizeInfo]::GetCompressedFileSizeFunction($_.FullName,$false)
+    Invoke-StorageSyncCloudTiering -Path $_.FullName
+}
+Write-Output "Tiered $i files"
---
+++ Windows-Explorer-Context-Menu/AzureFileSync-Tier-Files-Context-Menu.ps1
@@ -0,0 +1,15 @@
+﻿Param(
+    [Parameter(Mandatory=$True)]
+    [string]$FilePath
+)
+
+Import-Module "C:\Program Files\Azure\StorageSyncAgent\StorageSync.Management.ServerCmdlets.dll"
+If (Get-Module | Where { $_.Name -eq "StorageSync.Management.ServerCmdlets" }) {
+    if([System.IO.File]::Exists("$FilePath")){
+        Write-Output "Tiering file: $FilePath"
+        Invoke-StorageSyncCloudTiering -Path "$FilePath"
+    } else {
+        Write-Output "Could not find file: $FilePath"
+    }
+}
+Pause
